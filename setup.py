dee7aadf3baee1c5e6a2f5683485cd8a3d72d5e7
vitroid <vitroid@gmail.com>
2025-05-10 03:44:05 +0000

    Remove poetry references and simplify service configuration

--- setup.py
+++ setup.py
@@ -2,8 +2,6 @@ import os
 import subprocess
 from setuptools import setup, find_packages
 from setuptools.command.install import install
-from setuptools.command.develop import develop
-from setuptools.command.egg_info import egg_info
 
 class PostInstallCommand(install):
     def run(self):
@@ -19,51 +17,7 @@ Type=simple
 User=root
 Restart=always
 RestartSec=1
-ExecStart=/usr/local/bin/python3 -m loadmeters_api.api
-WorkingDirectory=/var/lib/loadmeters
-Environment=PORT=8081
-
-[Install]
-WantedBy=multi-user.target
-"""
-        
-        # サービスファイルを書き込み
-        service_path = "/etc/systemd/system/loadmeters.service"
-        with open(service_path, "w") as f:
-            f.write(service_content)
-        
-        # データディレクトリの作成
-        os.makedirs("/var/lib/loadmeters", exist_ok=True)
-        
-        # systemdの再読み込みとサービスの有効化
-        subprocess.run(["systemctl", "daemon-reload"])
-        subprocess.run(["systemctl", "enable", "loadmeters"])
-        subprocess.run(["systemctl", "start", "loadmeters"])
-
-class PostDevelopCommand(develop):
-    def run(self):
-        develop.run(self)
-        self._setup_service()
-
-class PostEggInfoCommand(egg_info):
-    def run(self):
-        egg_info.run(self)
-        self._setup_service()
-
-    def _setup_service(self):
-        # systemdサービスファイルの作成
-        service_content = """[Unit]
-Description=Load Meters Monitoring Service
-After=network.target
-
-[Service]
-Type=simple
-User=root
-Restart=always
-RestartSec=1
-Environment=POETRY_HOME=/opt/poetry
-Environment=PATH=/opt/poetry/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin
-ExecStart=/opt/poetry/bin/poetry run loadmeters
+ExecStart=/usr/bin/python3 -m loadmeters_api.api
 WorkingDirectory=/var/lib/loadmeters
 Environment=PORT=8081
 
@@ -112,8 +66,6 @@ setup(
     },
     cmdclass={
         'install': PostInstallCommand,
-        'develop': PostDevelopCommand,
-        'egg_info': PostEggInfoCommand,
     },
     author="Your Name",
     author_email="your.email@example.com",
