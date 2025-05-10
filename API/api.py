db7a1ac17f2877bc6f18ff020d171d8d0cb25474
vitroid <vitroid@gmail.com>
2025-05-10 03:28:50 +0000

    loadmetersサービスのsystemd設定を更新し、setup.pyにインストールスクリプトを追加。APIのエラーハンドリングを改善し、Pipfileを削除。新しいテストスクリプトを追加。

--- API/api.py
+++ API/api.py
@@ -9,8 +9,8 @@ import uvicorn
 from fastapi import Depends, FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi.staticfiles import StaticFiles
-from fastapi_utils.tasks import repeat_every
-
+from fastapi_utilities import repeat_every
+from logging import getLogger, basicConfig, INFO, DEBUG
 __api_version__ = 1
 
 from zeroconf import ServiceBrowser, ServiceListener, Zeroconf
@@ -52,13 +52,26 @@ class MyListener(ServiceListener):
 app = FastAPI()
 api = FastAPI(root_path=f"/v{__api_version__}")
 app.mount(f"/v{__api_version__}", api)
+# Svelte? UIもこいつがserveする。
 app.mount("/", StaticFiles(directory="../loadmeters/public", html=True), name="static")
 
 
+import http3
+
+client = http3.AsyncClient()
+
+async def call_api(url: str, **kwarg)->str:
+
+    r = await client.get(url, **kwarg)
+    return r.text
+
+from requests.exceptions import Timeout, ConnectionError
 
 @app.on_event("startup")
-@repeat_every(seconds=15)  # 15 seconds
-def update_history() -> None:
+@repeat_every(seconds=5)  # 5 seconds  調子が悪くなったら、これをコメントアウトし、exceptでキャッチできていないエラーをさぐる
+async def update_history() -> None:
+# def update_history() -> None:
+    logger = getLogger("uvicorn.app")
     fn = "servers.json"
     try:
         with open(fn) as f:
@@ -71,23 +84,43 @@ def update_history() -> None:
             stat = json.load(f)
     except:
         stat = dict()
+    logger.debug(servers)
+    for i, server in enumerate(servers):
+        logger.debug(f"{i}. {server}")
+    logger.debug("----")
+
     for server, info in servers.items():
+        logger.debug(server)
         headers = {"Accept": "application/json"}
         shortname = server.replace(".local.", "")
+        port = info["port"]
+        url = f'http://{server}:{port}/v1/info'
         try:
-            port = info["port"]
-            r = requests.get(f'http://{server}:{port}/v1/info', headers=headers)
+            logger.info(f"Try {url}")
+            r = requests.get(f'http://{server}:{port}/v1/info', headers=headers) #, timeout=2)
+            # r = await call_api(f'http://{server}:{port}/v1/info', headers=headers)
+            logger.debug(f"Status code {r.status_code}")
             data = r.json()
+            logger.info(f"  Obtained from {server}")
             if shortname not in stat:
                 stat[shortname] = dict()
             stat[shortname] = stat[shortname] | data
             stat[shortname]["address"] = info["addresses"]
             stat[shortname]["history"] = stat[shortname].get("history", [])
             stat[shortname]["history"].append(data["load"])
-        except:
-            stat[shortname]["history"].append(-1)
-        if len(stat[shortname]["history"]) > 60:
+        # except Timeout:
+        #     logger.info(f"Failed to get info from {server}.")
+        #     stat[shortname]["history"].append(-1)
+        except ConnectionError:
+            logger.info(f"  Failed to get info from {server}.")
+            if shortname in stat:
+                stat[shortname]["history"] = stat[shortname].get("history", [])
+                stat[shortname]["history"].append(-1)
+        finally:
+            pass
+        if shortname in stat and "history" in stat[shortname] and len(stat[shortname]["history"]) > 60:
             stat[shortname]["history"] = stat[shortname]["history"][-60:]
+    logger.info("dump.")
     with open(fn, "w") as f:
         json.dump(stat, f, indent=4)
 
@@ -117,8 +150,12 @@ async def load_average():
         return {}
 
 
-if __name__ == "__main__":
+def main():
+    basicConfig(level=INFO)
     zeroconf = Zeroconf()
     listener = MyListener()
     browser = ServiceBrowser(zeroconf, "_loadreporter._tcp.local.", listener)
-    uvicorn.run("api:app", host="0.0.0.0", reload=True, port=int(os.environ.get("PORT", 8088)) )
+    uvicorn.run("api:app", host="0.0.0.0", reload=False, port=int(os.environ.get("PORT", "8081")))
+
+if __name__ == "__main__":
+    main()
